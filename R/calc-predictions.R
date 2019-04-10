4b160bc6d9e25aca0709d0d21d5bf6c48c01c46b
Foster <Tim.Foster@SEPA.org.uk>
2019-04-10 10:08:20 +0100

    fix: remove NA from seasons to run for clarity and name parameters

--- R/calc-predictions.R
+++ R/calc-predictions.R
@@ -355,7 +355,7 @@ calcPredictions <- function(observed_values) {
   df_scores <- as.data.frame(getDFScores(final_predictors, df_coeff_gb685))
   #
   # Calculate the Mahanalobis disance of point x from site g for all referene sites
-  MahDist_g <- getMahDist(df_scores, df_mean_gb685)
+  MahDist_g <- getMahDist(DFscore = df_scores, meanvalues = df_mean_gb685)
   MahDistNames <- c("p1", "p2", "p3", "p4", "p5", "p6", "p7", "p8", "p9", "p10", "p11", "p12", "p13", "p14",
                     "p15", "p16", "p17", "p18", "p19", "p20", "p21", "p22", "p23", "p24", "p25", "p26", "p27", "p28",
                     "p29", "p30", "p31", "p32", "p33", "p34", "p35", "p36", "p37", "p38", "p39", "p40", "p41", "p42",
@@ -444,6 +444,7 @@ calcPredictions <- function(observed_values) {
                       "TL2_WHPT_NTAXA_AbW_CompFam", "TL2_WHPT_ASPT_AbW_CompFam")
 
   # Run the index Scores
+  seasons_to_run <- seasons_to_run[!is.na(seasons_to_run)]
   # data_to_bindTo, season_to_run, index_id, end_group_IndexDFrame
   mainData <- getSeasonIndexScores(data_to_bindTo = final_predictors_try3,
                                           season_to_run = seasons_to_run,

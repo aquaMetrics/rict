7878e42fb089d729a33b4e5671edf0873072e5ba
Foster <Tim.Foster@SEPA.org.uk>
2020-03-17 16:26:58 +0000

    tests: update test and reduce resources requirement

--- tests/testthat/test-rict-compare.R
+++ tests/testthat/test-rict-compare.R
@@ -18,12 +18,15 @@ test_that("rict_compare matches test data", {
     check.names = F
   )
 
-  a <-  rict(test_data[1:12, ], year_type = "single", store_eqrs = T)
-  b <-  rict(test_data[13:24, ], year_type = "single", store_eqrs = T)
+  a <-  rict(test_data[1:2, ], year_type = "single", store_eqrs = T)
+  b <-  rict(test_data[13:14, ], year_type = "single", store_eqrs = T)
   test <- rict_compare(a, b)
 
-  test_rict_compare <- test_rict_compare[, 2:45]
+  test_rict_compare <-
+    test_rict_compare[test_rict_compare$`Result A` %in% test$`Result A`, 2:45]
 
-  expect_equal(length(all.equal(test, test_rict_compare)), 3)
+  test <- type.convert(test)
+  test_rict_compare <- type.convert(test_rict_compare)
+  expect_equal(length(all.equal(test, test_rict_compare)), 1)
 
 })

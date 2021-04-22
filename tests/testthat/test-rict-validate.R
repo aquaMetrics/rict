f08ca9fe27bcf5e4c1636a43c16029fe642c19e9
fozy81 <fozy81@hotmail.com>
2021-04-22 15:12:33 +0100

    fix: Calculate the log10 values columns after replacement values have been added (otherwise 0 values become NaN/Inf). issue #13

--- tests/testthat/test-rict-validate.R
+++ tests/testthat/test-rict-validate.R
@@ -123,6 +123,14 @@ test_that("fails on some rows create fail messages (but process continues of val
   test <- rict_validate(test_data)
   test <- test$checks
   expect_equal(length(test$FAIL[test$FAIL != "---"]), 3)
+  # Test GIS values also value
+  test_data <- demo_gis_values_log
+  test_data$disch_cat[1] <- NA
+  test_data$disch_cat[2] <- 0
+  test_data$Altitude[1] <- -4
+  test <- rict_validate(test_data)
+  test <- test$checks
+  expect_equal(length(test$FAIL[test$FAIL != "---"]), 4)
 })
 
 # ---------------------------------------------------------------------
@@ -163,6 +171,19 @@ test_that("replacement values work if value is less than the ‘overall’ minim
   expect_equal(test[[1]][1, c("MEAN_DEPTH")], 1)
   expect_equal(test[[1]][1, c("ALKALINITY")], 0.1)
   expect_equal(test[[1]][1, c("SLOPE")], 0.1)
+
+  test_data <- demo_gis_values_log
+  test_data$Altitude[1] <- 0
+  test_data$d_f_source[1] <- 0.1
+  test_data$Alkalinity[1] <- 0.1
+  test_data$slope[1] <- 0
+  test <- rict_validate(test_data)
+  expect_equal(length(test[[2]][, 1]), 4)
+  expect_equal(test[[1]][1, c("ALTITUDE")], 1)
+  expect_equal(test[[1]][1, c("D_F_SOURCE")], 0.000100) # Now converted to Km from metres
+  expect_equal(test[[1]][1, c("ALKALINITY")], 0.1)
+  expect_equal(test[[1]][1, c("SLOPE")], 0.1)
+
 })
 
 # ---------------------------------------------------------------------

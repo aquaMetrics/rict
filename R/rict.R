2481143c417cdec474662ba67d6e5548a9c4b2a5
Foster <Tim.Foster@SEPA.org.uk>
2023-09-04 12:43:12 +0100

    fix: make rict() and rict_classify() the same
    set.seed was different in rict() to what has been manually tested in the RICT app which uses rict_classify(). Added test to ensure outputs are the same from both.

--- R/rict.R
+++ R/rict.R
@@ -17,7 +17,8 @@
 #' @param area Area is by detected by default from the NGR, but you can provide
 #'   the area parameter either 'iom', 'gb, 'ni' for testing purposes.
 #' @param crs optionally set crs to `29903` for Irish projection system.
-#' @param seed Use seed as setup in RICT2 Azure, useful for testing only.
+#' @param seed Use seed as setup in RICT2 Azure as default, useful for testing
+#'   only.
 #' @return Dataframe of classification results (and simulated EQRs if store_eqrs
 #'   = TRUE).
 #' @export
@@ -31,7 +32,7 @@ rict <- function(data = NULL, year_type = "multi",
                  store_eqrs = FALSE,
                  area = NULL,
                  crs = NULL,
-                 seed = FALSE) {
+                 seed = TRUE) {
   predictions <- rict_predict(data, area = area, crs = crs)
   results <- rict_classify(predictions, year_type, store_eqrs = store_eqrs, seed = seed)
   return(results)
